39957edfdaa53bcccc8a6632a225e23c576f530f
davidmunechika <davidam1203@gmail.com>
2021-03-24 21:00:21 -0400

    Restricting origin to client domain

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ if (process.env.NODE_ENV !== 'test') {
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(indexRouter);
